ce6ba79a20827023d023f51e002222f5f128cab3
nicity <maxim.mossienko@gmail.com>
2019-10-27 00:36:23 +0200

    Apache 2 license

--- src/com/advancedtools/cpp/actions/GotoBaseAction.java
+++ src/com/advancedtools/cpp/actions/GotoBaseAction.java
@@ -1,4 +1,4 @@
-/* AdvancedTools, 2007, all rights reserved */
+// Copyright 2006-2012 AdvancedTools. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.advancedtools.cpp.actions;
 
 import com.advancedtools.cpp.navigation.CppSymbolContributor;
--- src/com/advancedtools/cpp/facade/GotoSuperActionHandler.java
+++ src/com/advancedtools/cpp/facade/GotoSuperActionHandler.java
@@ -1,3 +1,4 @@
+// Copyright 2006-2012 AdvancedTools. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.advancedtools.cpp.facade;
 
 import com.intellij.lang.LanguageCodeInsightActionHandler;
